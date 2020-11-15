34bb21b1d0bed0bcfb23b4fb234196ff4d01680b
KostasKarakontis <73367769+konskara@users.noreply.github.com>
2020-11-15 02:01:21 +0200

    Add files via upload

---
+++ 2oSetAskisewn/Askisi3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ac_int.h>
+#include <ac_fixed.h>
+#include <ac_channel.h>
+#include <cstdlib>
+#include <ctime>
+
+void runlegth_encode(ac_channel<ac_int<4,false> > &in, ac_channel<ac_int<4,false> > &out){
+	ac_int<4, false> counter = 0;
+	ac_int<4, false> a[10];
+	
+}
