7b06bb2d66a9dd5113f565943a07bac95447876a
KostasKarakontis <73367769+konskara@users.noreply.github.com>
2020-11-15 20:53:31 +0200

    Update Askisi1.cpp

--- 3oSetAskisewn/Askisi1.cpp
+++ 3oSetAskisewn/Askisi1.cpp
@@ -4,3 +4,65 @@
 #include <ac_channel.h>
 #include <cstdlib>
 #include <ctime>
+
+static const int n = 10;
+
+typedef ac_int<4,false> dtype;
+typedef ac_channel<dtype> channel;
+
+class RunLengthEncoder{
+	private: 
+		dtype counter = 1;
+		dtype ifcounter;
+		dtype elsecounter;
+		dtype a[n];
+		dtype b[n];
+		dtype c[2*n];
+    public:
+	    stateRunLengthEncoder() {
+	    	b[n] = 0;
+	    	a[n] = 0;
+	    	c[2*n]=0;
+	    	counter = 1;
+	    	elsecounter =0;
+	    	ifcounter = 0;
+		}
+        void run (channel &in, channel &out) {
+        	if(in.available(1)) for(int i=0;i<n;i++) a[i] = in.read();
+        	for (int i=0;i<10;i++){
+		if(a[i] == a[i+1]){
+			counter = counter + 1;
+			ifcounter = ifcounter + 1;
+     	}
+	    else{
+	    	ac_int<5, false> fail = 2*(i-ifcounter);
+	    	c[fail] = a[i];
+	        b[i-ifcounter] = counter;
+	        elsecounter = elsecounter + 1;
+		    counter = 1;
+	    }
+	}   
+  	for (int i=0; i<elsecounter; i++){
+		c[2*i+1] = b[i];
+	}
+    for (int i=0;i<2*elsecounter;i++) out.write(c[i]);
+	    }
+};
+
+int main(){
+	RunLengthEncoder Q;
+    channel in;
+	channel out;
+	dtype a[n];
+	std::srand(std::time(0));	
+	for(int i=0;i<n;i++) {
+		a[i]=std::rand();
+		in.write(a[i]);
+	};
+	Q.run(in, out);
+    std::cout << "input: ";
+    for(int i=0; i<10;i++) std::cout<< a[i] << " ";
+    std::cout<<std::endl<<"output: ";
+    while(out.available(1)) std::cout << out.read() << " ";
+    std::cout<<std::endl;
+}
